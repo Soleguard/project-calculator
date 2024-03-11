cb5e93c10d12c50071d8560e21b58639dd371add
Soleguard <139895712+Soleguard@users.noreply.github.com>
2024-03-11 10:08:50 -0400

    Edit display and capitalize constants
    This version uses the value of the number 1
    variable for the display value. In addition, it
    uses an anonymous function instead of a named
    function for adding a value to the display screen;
    it appends the digits to the display value instead
    of changing the value when entering multiple
    digits; and it capitalizes all constants.

--- calculator.js
+++ calculator.js
@@ -1,66 +1,80 @@
 // Declare and initialize display value
+// Represents content displayed on screen (before/after equal sign click) (appendable)
 let display_Value = '';
 
+// Declare and initialize numbers 1 and 2 and result
+let number_1 = '';
+let number_2 = '';
+
+// Represents final solution of all operations
+let solution = 0;
+
 // Select display screen
-const screen = document.getElementById("screen");
+const SCREEN = document.getElementById("screen");
 
 // Select all digit buttons
-const digits = document.querySelectorAll(".digit");
+const DIGITS = document.querySelectorAll(".digit");
+
+// Select all signs
+const SIGNS = document.querySelectorAll(".sign");
 
 // Loop through each digit button, adding a click event listener
-digits.forEach(function(digit) {
-  // Invoke add_Value function upon click
-  digit.addEventListener("click", add_Value);
-});
+DIGITS.forEach(function(digit) {
+  // Invoke function upon click
+  digit.addEventListener("click", function() {
+  
+    // Append to Number 1
+    number_1 = number_1 + digit.innerHTML;
 
-// Create function for adding a value to display screen
-function add_Value(e) {
-  // Set display value to digit value
-  // Error: Returns "undefined"
-  // Expected Result: HTML text of button (ex. 1)
-  display_Value = e;
-  screen.innerHTML = display_Value;
-};
+    // Append Number 1 to display_Value
+    display_Value = number_1;
+
+    // Display display_Value on screen
+    SCREEN.innerHTML = display_Value;
+  });
+});
 
 // Declare add function
 const add = function(num_1, num_2) {
     return num_1 + num_2;
   };
   
-  // Declare subtract function
-  const subtract = function(num_1, num_2) {
-    return num_1 - num_2;
-  };
+// Declare subtract function
+const subtract = function(num_1, num_2) {
+  return num_1 - num_2;
+};
   
-  // Declare multiply function
-  const multiply = function(num_1, num_2) {
-    return num_1 * num_2;
-  };
+// Declare multiply function
+const multiply = function(num_1, num_2) {
+  return num_1 * num_2;
+};
   
-  // Declare divide function
-  const divide = function(num_1, num_2) {
-    return num_1 / num_2;
-  };
+// Declare divide function
+const divide = function(num_1, num_2) {
+  return num_1 / num_2;
+};
   
-  // Declare operate function
-  const operate = function(num_1, num_2, operator) {
-    // Determine addition
-    if (operator === "+") {
-      return add(num_1, num_2);
-    }
-    
-    // Determine subtraction
-    else if (operator === "-") {
-      return subtract(num_1, num_2);
-    }
-    
-    // Determine multiplication
-    else if (operator === "*") {
-      return multiply(num_1, num_2);
-    }
-    
-    // Determine division
-    else if (operator === "/") {
-      return divide(num_1, num_2);
-    }
-  };
+// Declare operate function
+const operate = function(num_1, num_2, operator) {
+  
+  // Determine addition
+  if (operator === "+") {
+    return add(num_1, num_2);
+  }
+
+  // Determine subtraction
+  else if (operator === "-") {
+    return subtract(num_1, num_2);
+  }
+
+  // Determine multiplication
+  else if (operator === "*") {
+    return multiply(num_1, num_2);
+  }
+
+  // Determine division
+  else if (operator === "/") {
+    return divide(num_1, num_2);
+  }
+  
+};
